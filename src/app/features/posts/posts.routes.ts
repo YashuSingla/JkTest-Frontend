544dbabd0bf1922f07d7cf6df4ad82f0ffd9ed2c
YashuSingla <53873204+YashuSingla@users.noreply.github.com>
2025-04-14 16:49:16 +0530

    Update posts.routes.ts

--- src/app/features/posts/posts.routes.ts
+++ src/app/features/posts/posts.routes.ts
@@ -16,6 +16,7 @@ export const routes: Routes = [
   },
   {
     path: ':id',
+    canActivate: [authGuard],
     loadComponent: () =>
       import('./post-detail/post-detail.component').then((m) => m.PostDetailComponent),
   },
